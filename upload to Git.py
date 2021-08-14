016ac6f970a4f0d9fa18975282a90227ed68431d
137845 <55562929+137845@users.noreply.github.com>
2021-08-14 15:16:28 -0500

    Add files via upload

---
+++ upload to Git.py
@@ -0,0 +1,12 @@
+import boto3
+
+def describe_ec2_instance():
+    try:
+        print("Describe EC2 instance")
+        resource_ec2 = boto3.client("ec2")
+        print(resource_ec2.describe_instance()["Reservation"][0]["Instance"][0]["InstanceId"])
+    except Exception as e:
+        print(e)
+
+describe_ec2_instance()
+    
