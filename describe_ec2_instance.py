417e472c5c2552cc5eda7e6298b95a230bf13c86
EC2 Default User <ec2-user@ip-172-31-79-113.ec2.internal>
2021-08-15 02:31:58 +0000

    describe_ec2_instance

--- describe_ec2_instance.py
+++ describe_ec2_instance.py
@@ -4,7 +4,7 @@ def describe_ec2_instance():
     try:
         print("Describe EC2 instance")
         resource_ec2 = boto3.client("ec2")
-        print(resource_ec2.describe_instance()["Reservations"][0]["Instances"][0]["InstanceId"])
+        print(resource_ec2.describe_instances()["Reservations"][0]["Instances"][0]["InstanceId"])
     except Exception as e:
         print(e)
 
