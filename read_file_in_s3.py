84a3d6191038e6ba25a983f1dc62121b7b326055
137845 <55562929+137845@users.noreply.github.com>
2021-08-19 15:47:04 -0500

    Update read_file_in_s3.py

--- read_file_in_s3.py
+++ read_file_in_s3.py
@@ -1,8 +1,4 @@
+import boto3
 s3 = boto3.resource('s3')
-bucket = s3.Bucket('qi-s3-bucket-test101')
-# Iterates through all the objects, doing the pagination for you. Each obj
-# is an ObjectSummary, so it doesn't contain the body. You'll need to call
-# get to get the whole body.
-for obj in bucket.objects.all():
-    key = obj.key
-    body = obj.get()['Body'].read()
+obj = s3.Object('qi-s3-bucket-test101', 'newtest.txt')
+body = obj.get()['Body'].read()
